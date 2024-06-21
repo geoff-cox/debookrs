f55ddb8aaf73b30328b588c04cdb358848dc8fc8
geoff-cox <geoffreywcox@gmail.com>
2024-06-21 19:23:57 -0400

    Major update to the solutions chapter

--- assets/code/family-of-solutions.js
+++ assets/code/family-of-solutions.js
@@ -1,17 +1,17 @@
 /* 
 Interactive Script illustrating the concepts of
 	> Family of Solutions
-	> Initial Conditions
 	> Particular Solutions
 */
 
-var box_x_L = -3.1, box_x_R = 3.1, box_y_B = -2, box_y_T = 7;
+// Define the x and y ranges for the board
+var x_L = -3.1, x_R = 3.1, y_B = -2, y_T = 7;
 
 // Create the JSXGraph board
 var board = JXG.JSXGraph.initBoard(
 	'interactive-family-slate',
 	{
-		boundingbox:[box_x_L,box_y_T,box_x_R,box_y_B],
+		boundingbox:[x_L,y_T,x_R,y_B],
 		keepaspectratio:false,
 		axis:true,
 		defaultAxes: {
@@ -23,47 +23,49 @@ var board = JXG.JSXGraph.initBoard(
 	}
 );
 
-// Plot a static subset from the family of solutions at each specified y-value
-const y_list = [];
-for (let i = box_y_B+1; i <= 3; i += 1) {
-	y_list.push(i);
-}
-y_list.push(2.5);
-y_list.push(3.5);
-for (let i = 4; i <= box_y_T-1; i += 1) {
-	y_list.push(i);
-}
-const c_list   = [  -4,   -3,   -2,   -1,  -.5,    0,   .5,    1,    2,    3];
-const c_list_x = [ 0.2, 0.25, 0.30, 0.35, 0.37, 0.70, 0.45, 0.43, 0.34, 0.29];
-const c_list_y = [-1.6, -0.6, 0.60, 1.60, 2.25, 2.95, 3.70, 4.30, 5.35, 6.40];
+// Added because the board is too small in pretext's side-by-side environment
+// Comment out or adjust as needed
+board.resizeContainer(300, 300);
 
-for (let i=0; i<y_list.length; i++) {
-	var f_family = function(x){ return (y_list[i] - 3)*Math.exp(x*x)+3; };
-	board.create(
-		'functiongraph',
-		[f_family, box_x_L, box_x_R],
-		{strokecolor:'green', strokeOpacity:0.35, strokeWidth:3}
-	);
-
-	board.create(
-		'text',
-		[c_list_x[i], c_list_y[i], c_list[i]],
-		{fontSize: 18, strokecolor: 'blue'}
-	);
-}
-
-var txt = board.create('text', [-1, 1, 'C'], {
-    fontSize: 24,
+// Placeholder text area for the function labels
+var txt = board.create('text', [0.9, 3.35, ''], {
+    fontSize: 16,
     visible: false
 });
 
-plot_soln.on('over', (evt) => {
-	// Display text
-    txt.setAttribute({visible: true});
-});
-plot_soln.on('out', (evt) => {
-	// Hide text
-	txt.setAttribute({visible: false});
-});
+// Hardcode a subset of the family of solutions' y-intercepts and corresponding constants
+const y_list 	= [  -1,    0,    1,    2,  2.5,    3,  3.5,    4,    5,    6];
+const c_list	= [  -4,   -3,   -2,   -1,  -.5,    0,   .5,    1,    2,    3];
+
+// Create the family of solution objects
+const f_str = [];
+const f_list = [];
+const f_plot = [];
+
+for (let i=0; i<y_list.length; i++) {
+
+	// Define the family of solutions arrays
+	f_str.push('y = ' + c_list[i] + 'e^{x^2} + 3');
+	f_list.push(function(x){ return (y_list[i] - 3)*Math.exp(x*x)+3; });
+	f_plot.push(board.create(
+		'functiongraph',
+		[f_list[i], x_L, x_R],
+		{strokecolor:'green', strokeOpacity:0.35, strokeWidth:3}
+	));
+
+	// Dynamic text labels with mouse-over
+	f_plot[i].on('over', (evt) => {
+		txt.setAttribute({visible: true});
+		txt.setText(f_str[i]);
+	});
+	f_plot[i].on('out', (evt) => {
+		txt.setAttribute({visible: false});
+		txt.setText(f_str[i]);
+	});
+}
+
+
+
+
 
 
--- assets/code/initial-condition.js
+++ assets/code/initial-condition.js
@@ -5,66 +5,59 @@ Interactive Script illustrating the concepts of
 	> Particular Solutions
 */
 
-var box_x_L = -3.1, box_x_R = 3.1, box_y_B = -2, box_y_T = 7;
+// Define the x and y ranges for the board
+var x_L = -3.1, x_R = 3.1, y_B = -2, y_T = 7;
 
 // Create the JSXGraph board
 var board = JXG.JSXGraph.initBoard(
 	'interactive-drag-ic-slate',
 	{
-		boundingbox:[box_x_L,box_y_T,box_x_R,box_y_B],
+		boundingbox:[x_L,y_T,x_R,y_B],
 		keepaspectratio:false,
 		axis:true,
 		defaultAxes: {
-			x: {ticks: {label: {fontsize: 16}}},
-			y: {ticks: {label: {fontsize: 16}}},
+			x: {ticks: {insertTicks: false, ticksDistance: 1, label: {fontsize: 14}}},
+			y: {ticks: {label: {fontsize: 14}}},
 		},
 		showNavigation:false,
 		showCopyright:false
 	}
 );
 
-// Plot a static subset from the family of solutions at each specified y-value
-const y_list = [];
-for (let i = box_y_B; i <= 3; i += 0.5) {
-	y_list.push(i);
-}
-y_list.push(2.75);
-y_list.push(2.90);
-y_list.push(3.10);
-y_list.push(3.25);
-for (let i = 3.5; i <= box_y_T; i += 0.5) {
-	y_list.push(i);
-}
-// const y_list = [-3, -2.5, -2, -1.5, -1, -0.5, 0, 0.5, 1, 1.5, 2, 2.3, 2.5, 2.8, 2.9, 2.99, 2.999, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7];
-var x_i = 0;
-var draw = true;
+// Added because the board is too small in pretext's side-by-side environment
+// Comment out or adjust as needed
+board.resizeContainer(300, 300);
+
+// Hardcode a subset of the family of solutions' y-intercepts and corresponding constants
+const y_list 	= [  -1,    0,    1,    2,  2.5,    3,  3.5,    4,    5,    6];
+const c_list	= [  -4,   -3,   -2,   -1,  -.5,    0,   .5,    1,    2,    3];
+
+// Create the family of solution objects
+const f_str = [];
+const f_list = [];
+const f_plot = [];
+
 for (let i=0; i<y_list.length; i++) {
-	var f_family = function(x){ return (y_list[i] - 3)*Math.exp(x*x)+3; };
-	board.create(
+
+	// Define the family of solutions arrays
+	f_str.push('y = ' + c_list[i] + 'e^{x^2} + 3');
+	f_list.push(function(x){ return (y_list[i] - 3)*Math.exp(x*x)+3; });
+	f_plot.push(board.create(
 		'functiongraph',
-		[f_family, box_x_L, box_x_R],
+		[f_list[i], x_L, x_R],
 		{strokecolor:'green', strokeOpacity:0.35, strokeWidth:3}
-	);
-	// if (draw) {
-	// 	board.create(
-	// 		'text',
-	// 		[x_i, f_family(x_i) ,function(){ return (y_list[i] - 3).toFixed(1); }],
-	// 		{fontSize: 12}
-	// 	);
-	// }
-	// draw = !draw;
-	// x_i += 0.1;
+	));
 }
 
 // Create the interactive slider along the y-axis
 var y_slider = board.create(
 	'slider',
-	[[0,box_y_B+1],[0,box_y_T-1],[box_y_B+1,1,box_y_T-1]],
+	[[0,y_B+1],[0,y_T-1],[y_B+1,2,y_T-1]],
 	{ 	// Slider Properties
 		withLabel: false,
 		size: 5,
 		highline: {strokeWidth: 0},
-		snapWidth: 0.05
+		snapWidth: 0.1
 	}
 );
 
@@ -76,19 +69,18 @@ var f0 = function(x){
 // Update the particular solution
 var plot_soln = board.create(
 	'functiongraph',
-	[f0, box_x_L, box_x_R],
+	[f0, x_L, x_R],
 	{strokecolor: 'blue', strokeOpacity: 0.6, strokeWidth: 4}
 );
 
 var label_x = 1.6;
 	label_y = 3.7;
 
-
-var label_line = function(x){ return ( (label_y - y_slider.Value())/label_x )*x + y_slider.Value(); }
+var label_line = function(x){ return ( (3.3 - y_slider.Value())/(-1.7) )*x + y_slider.Value(); }
 
 board.create(
 	'functiongraph',
-	[label_line, 0, label_x],
+	[label_line, -1.6, 0],
 	{
 		strokecolor:	'black', 
 		strokeOpacity:	1, 
@@ -97,30 +89,33 @@ board.create(
 	
 board.create(
 	'text',
-	[label_x + 0.1, label_y, function(){ return 'Initial Condition:' }],
+	[-3, 3.8, function(){ return 'Initial Condition:' }],
+	{fontSize: 13}
+);
+board.create(
+	'text',
+	[-3, 3.3, function(){ return 'y(0) = ' + (y_slider.Value()).toFixed(1); }],
 	{fontSize: 14}
 );
 board.create(
 	'text',
-	[label_x + 0.1, label_y - 0.5, function(){ return 'y(0) = ' + (y_slider.Value()).toFixed(2); }],
-	{fontSize: 16}
+	[1.5, 4.4, function(){ return 'Particular Solution:' }],
+	{fontSize: 13}
 );
 board.create(
 	'text',
-	[label_x + 0.1, label_y - 1.1, function(){ return 'Particular Solution:' }],
+	[1.1, 3.6 ,function(){ 
+		if (y_slider.Value() < 3) {
+			return 'y = ' + (y_slider.Value() - 3).toFixed(1) + 'e^{x^2} + 3';
+		} else {
+			return 'y =  ' + (y_slider.Value() - 3).toFixed(1) + 'e^{x^2} + 3';
+		};
+	}],
 	{fontSize: 14}
 );
+
 board.create(
 	'text',
-	[label_x + 0.1, label_y - 1.5 ,function(){ return 'y = ' + (y_slider.Value() - 3).toFixed(2) + 'e^{x^2} + 3'; }],
-	{fontSize: 16}
-);
-var 
-	from	= board.create('point', [label_x + 0.6, label_y - 2.1], { visible: false });
-	to 		= board.create('point', [label_x + 0.6, label_y - 1.8], { visible: false });
-board.create('arrow', [from, to]);
-board.create(
-	'text',
-	[label_x + 0.55, label_y - 2.3 ,function(){ return 'c_1'; }],
-	{fontSize: 16}
+	[-2.65, 2.6 ,function(){ return 'c = ' + (y_slider.Value() - 3).toFixed(1); }],
+	{fontSize: 14}
 );
